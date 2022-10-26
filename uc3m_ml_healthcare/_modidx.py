01f109fecad99ecda28cf05d5269901f6f4dd9a1
Manuel A. Vázquez <manuavazquez@gmail.com>
2022-10-26 09:40:36 +0200

    - added "30_train.ipynb"

--- uc3m_ml_healthcare/_modidx.py
+++ uc3m_ml_healthcare/_modidx.py
@@ -20,4 +20,12 @@ d = { 'settings': { 'branch': 'main',
                                                                                             'uc3m_ml_healthcare/data.py')},
             'uc3m_ml_healthcare.generate_timeseries': {},
             'uc3m_ml_healthcare.plot': { 'uc3m_ml_healthcare.plot.partially_observed_time_series': ( 'plot.html#partially_observed_time_series',
-                                                                                                     'uc3m_ml_healthcare/plot.py')}}}
+                                                                                                     'uc3m_ml_healthcare/plot.py')},
+            'uc3m_ml_healthcare.train': { 'uc3m_ml_healthcare.train.LatentODELoss': ( 'train.html#latentodeloss',
+                                                                                      'uc3m_ml_healthcare/train.py'),
+                                          'uc3m_ml_healthcare.train.LatentODELoss.__call__': ( 'train.html#latentodeloss.__call__',
+                                                                                               'uc3m_ml_healthcare/train.py'),
+                                          'uc3m_ml_healthcare.train.LatentODELoss.__init__': ( 'train.html#latentodeloss.__init__',
+                                                                                               'uc3m_ml_healthcare/train.py'),
+                                          'uc3m_ml_healthcare.train.LatentODELoss.__str__': ( 'train.html#latentodeloss.__str__',
+                                                                                              'uc3m_ml_healthcare/train.py')}}}
